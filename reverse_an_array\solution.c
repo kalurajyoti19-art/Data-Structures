17d843e7c85f48e81a53bd1c8e0b8c629b6bd0c5
kalurajyoti19-art <kalurajyoti19@gmail.com>
2026-08-21 07:30:38 +0530

    Implement array reversal and clean up code
    Removed unnecessary calculations for sum, average, max, and min. Implemented a function to reverse an array based on user input.

--- reverse_an_array\solution.c
+++ reverse_an_array\solution.c
@@ -1,37 +1,34 @@
 Try AI directly in your favorite apps … Use Gemini to generate drafts and refine content, plus get Gemini Pro with access to Google's next-gen AI for ₹1,950 ₹489 for 3 months
 1
 100%
-◾FINDING MAXIMUM, MINIMUM,SUM AND AVERAGE 
+◾REVERSING AN ARRAY
 #include<stdio.h>
+void reverse(int arr[],int n){
+int l=0;
+int r=n-1;
+while(l<r)
+{
+int temp=arr[l];
+arr[l]=arr[r];
+arr[r]=temp;
+l++;
+r--;
+}
 int main(){
-int arr[limit]; int limit;
-float avg=0.0;
-int sum;
-int max;
-int min;
-printf("Enter the number limit:\n");
-scanf("%d",&limit);
+int arr[n];
+int n;
+printf("Enter the elements of the array:\n");
+scanf("%d",&n);
 printf("Input Elements:\n");
-for(int i=0;i<limit;i++);
+for(int i=0;i<n;i++)
 {
 scanf("%d",&arr[i]);
 }
-sum=arr[0];
-max=arr[0];
- min=arr[0];
-for(int i=1;i<limit;i++);
-if(arr[i]>max)
+reverse(arr,n);
+
+for(int i=0;i<n;i++)
 {
-max=arr[i];
+printf("%d",arr[i]);
 }
-if(arr[i]<min)
-{
-min=arr[i];
-}
-sum=sum+arr[i];
-}
-printf("\nThe sum of the numbers is:%f",sum);
-printf ("\nThe average of the numbers is:%f",sum/limit);
-printf("\nThe maximum number is:%f",max);
-printf("\nThe minimum number is:%f",min);
+return 0;
 }
